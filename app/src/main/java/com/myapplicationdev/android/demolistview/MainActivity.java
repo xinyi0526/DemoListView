7b9383a174a26c26f113fe54fa0ab0a8f902d0a6
xinyi0526 <xinyi.luvand620@gmail.com>
2020-04-30 11:28:31 +0800

    Submission

--- app/src/main/java/com/myapplicationdev/android/demolistview/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demolistview/MainActivity.java
@@ -3,8 +3,11 @@ package com.myapplicationdev.android.demolistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -31,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
         //each row and the food String array together
         aa = new FoodAdapter(this,R.layout.row,food);
         lv.setAdapter(aa);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Food selectedFood = food.get(position);
+                Toast.makeText(MainActivity.this,selectedFood.getName()+"Star:"+selectedFood.isStar(),Toast.LENGTH_LONG).show();
+            }
+        });
 
 
     }
