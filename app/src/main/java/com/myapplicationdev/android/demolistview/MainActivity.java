8083d64ad4bd5e416a3ba2a72df6f625019c7af7
xinyi0526 <xinyi.luvand620@gmail.com>
2020-04-30 11:24:06 +0800

    Submission

--- app/src/main/java/com/myapplicationdev/android/demolistview/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/demolistview/MainActivity.java
@@ -3,12 +3,35 @@ package com.myapplicationdev.android.demolistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    ArrayAdapter aa;
+    ArrayList<Food> food;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = (ListView) this.findViewById(R.id.lvFood);
+
+        //Create a few food objects in food array
+        food = new ArrayList<Food>();
+        food.add(new Food("Ah Lat Coffee",false));
+        food.add(new Food("Rocky choc",true));
+        food.add(new Food("Kid Cat Choc",true));
+
+        //Link this Activity object, the row.xml layout for
+        //each row and the food String array together
+        aa = new FoodAdapter(this,R.layout.row,food);
+        lv.setAdapter(aa);
+
+
     }
 }
